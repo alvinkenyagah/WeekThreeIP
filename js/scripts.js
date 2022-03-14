b1e2c11a0847383646662d32c90e5f6cf968ec42
Alvinkenyagah <alvin47k@gmail.com>
2022-03-14 19:54:29 +0300

    Add smooth scrolling effect

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 $(".snake").snakeify({
   speed: 50,
 });
+
+//smooth scroll using #target
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
